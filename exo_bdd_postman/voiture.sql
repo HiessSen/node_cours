9414ff7ffc570b9ac15138ddbc0594a6c73dd233
HiessSen <118723696+HiessSen@users.noreply.github.com>
2023-05-04 13:03:47 +0200

    Ajout du fichier SQL

---
+++ exo_bdd_postman/voiture.sql
@@ -0,0 +1,53 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : 127.0.0.1:3306
+-- Généré le : jeu. 04 mai 2023 à 11:03
+-- Version du serveur : 8.0.31
+-- Version de PHP : 8.0.26
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données : `garage`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `voiture`
+--
+
+DROP TABLE IF EXISTS `voiture`;
+CREATE TABLE IF NOT EXISTS `voiture` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `marque` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
+  `modele` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
+  `kilometres` int NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Déchargement des données de la table `voiture`
+--
+
+INSERT INTO `voiture` (`id`, `marque`, `modele`, `kilometres`) VALUES
+(1, 'Lamborghini', 'Gallardo', 12000),
+(2, 'Renault', '205', 280000),
+(3, 'undefined', 'undefined', 0),
+(4, 'undefined', 'undefined', 0),
+(5, 'Wayne Enterprise', 'Batmobile', 780000);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
